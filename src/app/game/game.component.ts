cbce3f8805f9035bc6eafbfbf735bff64fb2fa23
Stefan Behrendt <stefan.behrendt@sparkfountain.de>
2019-05-01 10:42:52 +0200

    work in progress

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,6 +1,7 @@
-import { Component, ViewChild, AfterViewInit, ElementRef, HostListener } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 // tslint:disable-next-line: max-line-length
-import { Engine, Scene, Camera, ArcRotateCamera, Vector3, HemisphericLight, PointLight, MeshBuilder, StandardMaterial, CubeTexture, Texture } from '@babylonjs/core';
+import { ArcRotateCamera, Camera, CubeTexture, Engine, MeshBuilder, Scene, StandardMaterial, Texture, Vector3, SceneLoader, AssetsManager, MeshAssetTask, AbstractMesh, Color3, Mesh } from '@babylonjs/core';
+import '@babylonjs/loaders/OBJ';
 
 @Component({
   selector: 'app-game',
@@ -14,6 +15,8 @@ export class GameComponent implements AfterViewInit {
   public scene: Scene;
   public camera: Camera;
 
+  public assetsManager: AssetsManager;
+
   @HostListener('window:resize', ['$event'])
   onResize() {
     this.engine.resize();
@@ -25,11 +28,6 @@ export class GameComponent implements AfterViewInit {
     this.engine = new Engine(this.canvas.nativeElement, true); // Generate the BABYLON 3D engine
     this.createScene(); // Call the createScene function
     this.createSkyBox();
-
-    // Register a render loop to repeatedly render the scene
-    this.engine.runRenderLoop(() => {
-      this.scene.render();
-    });
   }
 
   createScene(): void {
@@ -38,15 +36,36 @@ export class GameComponent implements AfterViewInit {
     this.camera = new ArcRotateCamera('Camera', (Math.PI / 4), (Math.PI / 4), 5, Vector3.Zero(), this.scene);
     this.camera.attachControl(this.canvas.nativeElement, true);
 
+    this.assetsManager = new AssetsManager(this.scene);
+    let meshTask = this.assetsManager.addMeshTask("skull task", "", "/assets/objects/", "palmtree01.obj");
 
+    meshTask.onSuccess = (task: MeshAssetTask) => {
+      task.loadedMeshes.forEach((mesh: Mesh) => {
+        mesh.position = new Vector3(0, -10, 10);
+        mesh.scaling = new Vector3(0.1, 0.1, 0.1);
+        console.info(mesh.material);
+        mesh.material.ambientColor = new Color3(1, 1, 1);
+      });
+    }
+
+    this.assetsManager.onFinish = (tasks) => {
+      // Register a render loop to repeatedly render the scene
+      this.engine.runRenderLoop(() => {
+        this.scene.render();
+      });
+    };
+
+    this.assetsManager.load();
   }
 
   createSkyBox() {
-    const skybox = MeshBuilder.CreateBox('skyBox', { size: 1.0 }, this.scene);
+    const skybox = MeshBuilder.CreateBox('skyBox', { size: 1000.0 }, this.scene);
     const skyboxMaterial = new StandardMaterial('skyBox', this.scene);
     skyboxMaterial.backFaceCulling = false;
     skyboxMaterial.reflectionTexture = new CubeTexture('/assets/textures/TropicalSunnyDay', this.scene);
     skyboxMaterial.reflectionTexture.coordinatesMode = Texture.SKYBOX_MODE;
+    /* skyboxMaterial.diffuseColor = new Color3(0, 0, 0);
+    skyboxMaterial.specularColor = new Color3(0, 0, 0); */
     skyboxMaterial.disableLighting = true;
     skybox.material = skyboxMaterial;
   }
