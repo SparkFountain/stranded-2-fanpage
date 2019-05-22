6806d9772d26d21f9d1aac733af8075d726fd742
Stefan Behrendt <behrendt@onexip.com>
2019-05-22 17:32:25 +0200

    added english welcome text and headline css

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-news',
@@ -7,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsComponent implements OnInit {
 
-  constructor() { }
+  constructor(public translateService: TranslateService) {
+  }
 
   ngOnInit() {
   }
