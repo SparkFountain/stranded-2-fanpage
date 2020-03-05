e2ddaa9b5cfb08d502d5694a29c8474ba5e60b0a
Stefan Behrendt <behrendt@onexip.com>
2020-03-05 15:59:52 +0100

    added imprint, disclaimer, links stubs

---
+++ src/app/links/links.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { LinksComponent } from './links.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LinksComponent
+  }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, RouterModule.forChild(routes)]
+})
+export class LinksModule {}
